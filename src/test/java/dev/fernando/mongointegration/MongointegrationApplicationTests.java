1078db43ce4b8e45dd6a2ad6c67992108bfd5494
Fernando Pérez <jumbonionga+git@gmail.com>
2024-01-30 13:48:58 -0600

    Initial commit

---
+++ src/test/java/dev/fernando/mongointegration/MongointegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernando.mongointegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongointegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
